e40f0d78bfa127744a1236baa1bc3ad4b138d492
Jinwoo Jung <wlsdn2749@gmail.com>
2025-08-13 11:18:29 +0900

    Update: Missing Dll third_party/openssl/...

---
+++ Common/Protobuf/bin/ClientPacketHandler.h
@@ -0,0 +1,76 @@
+#pragma once
+#include "Protocol.pb.h"
+
+
+using PacketHandlerFunc = std::function<bool(PacketSessionRef&, BYTE*, int32)>;
+extern PacketHandlerFunc GPacketHandler[UINT16_MAX]; // 65535만큼의 배열 개수
+
+enum : uint16
+{
+	PKT_C_JwtLoginRequest = 1000,
+	PKT_S_JwtLoginReply = 1001,
+	PKT_C_CreateCharacterRequest = 1002,
+	PKT_S_CreateCharacterReply = 1003,
+
+};
+
+// Custom Handler : 직접 컨텐츠 작업자가 CPP를 만들어야함
+
+bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len);
+bool Handle_C_JwtLoginRequest(PacketSessionRef& session, Protocol::C_JwtLoginRequest& pkt);
+bool Handle_C_CreateCharacterRequest(PacketSessionRef& session, Protocol::C_CreateCharacterRequest& pkt);
+
+class ClientPacketHandler
+{
+public:
+
+	static void init()
+	{
+		for (int32 i = 0; i < UINT16_MAX; i++)
+		{
+			GPacketHandler[i] = Handle_INVALID;
+		}
+		GPacketHandler[PKT_C_JwtLoginRequest] = [](PacketSessionRef& session, BYTE* buffer, int32 len) {return HandlePacket<Protocol::C_JwtLoginRequest>(Handle_C_JwtLoginRequest, session, buffer, len); };
+		GPacketHandler[PKT_C_CreateCharacterRequest] = [](PacketSessionRef& session, BYTE* buffer, int32 len) {return HandlePacket<Protocol::C_CreateCharacterRequest>(Handle_C_CreateCharacterRequest, session, buffer, len); };
+		
+	}
+	static bool HandlePacket(PacketSessionRef& session, BYTE* buffer, int32 len)
+	{
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+		return GPacketHandler[header->id](session, buffer, len);
+	}
+	static SendBufferRef MakeSendBuffer(Protocol::S_JwtLoginReply& pkt) { return MakeSendBuffer(pkt, PKT_S_JwtLoginReply); };
+	static SendBufferRef MakeSendBuffer(Protocol::S_CreateCharacterReply& pkt) { return MakeSendBuffer(pkt, PKT_S_CreateCharacterReply); };
+
+private:
+	template<typename PacketType, typename ProcessFunc>
+	static bool HandlePacket(ProcessFunc func, PacketSessionRef& session, BYTE* buffer, int32 len)
+	{
+		PacketType pkt;
+		if (pkt.ParseFromArray(buffer + sizeof(PacketHeader), len - sizeof(PacketHeader)) == false)
+			return false;
+
+		return func(session, pkt);
+	}
+
+	template<typename T>
+	static SendBufferRef MakeSendBuffer(T& pkt, uint16 pktId)
+	{
+		const uint16 dataSize = static_cast<uint16>(pkt.ByteSizeLong()); // byte로 환산하면 얼마인가?
+
+		const uint16 packetSize = dataSize + sizeof(PacketHeader);
+
+		SendBufferRef sendBuffer = GSendBufferManager->Open(packetSize);
+
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(sendBuffer->Buffer());
+		header->size = packetSize;
+		header->id = pktId;
+
+		ASSERT_CRASH(pkt.SerializeToArray(&header[1], dataSize));
+
+
+		sendBuffer->Close(packetSize);
+		return sendBuffer;
+	}
+
+};
